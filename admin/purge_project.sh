dbc2c568b55308bda128b9e76b7e2179c832632e
jbride <jbride@ratwater.com>
2015-09-04 16:06:58 -0400

    latest

--- admin/purge_project.sh
+++ admin/purge_project.sh
@@ -1,3 +1,9 @@
+echo -en "\n***** Delete DeploymentConfig resources\n"
+oc delete dc --all
+
+echo -en "\n***** Delete ReplicationController resources\n"
+oc delete rc --all
+
 echo -en "\n***** Delete BuildConfig resources\n"
 oc delete bc --all
 
@@ -6,12 +12,6 @@ oc delete is --all
 
 echo -en "\n***** Delete build resources\n"
 oc delete build --all
-
-echo -en "\n***** Delete DeploymentConfig resources\n"
-oc delete dc --all
-
-echo -en "\n***** Delete ReplicationController resources\n"
-oc delete rc --all
 
 echo -en "\n***** Delete service resources\n"
 oc delete service --all
@@ -22,14 +22,14 @@ oc delete pod --all
 echo -en "\n***** Delete Routes\n"
 oc delete route --all
 
-echo -en "\n***** Delete PersistentVolumeClaims\n"
-oc delete pvc --all
-
 echo -en "\n***** Delete Service Accounts\n"
 oc delete sa --all
 
 echo -en "\n***** Delete Secrets\n"
 oc delete secrets --all
+
+echo -en "\n***** Delete PersistentVolumeClaims\n"
+oc delete pvc --all
 
 echo -en "\n**** refreshed policies\n"
 oc policy add-role-to-user admin system:serviceaccount:devops:default
